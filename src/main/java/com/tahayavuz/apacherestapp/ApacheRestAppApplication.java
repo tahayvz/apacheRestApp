afa79bd12908b2dbfb848596e8bf10808d2b0db7
Taha Yavuz <45938952+tahayvz@users.noreply.github.com>
2021-06-27 21:50:51 +0300

    first commit

---
+++ src/main/java/com/tahayavuz/apacherestapp/ApacheRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.tahayavuz.apacherestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheRestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tahayavuz/apacherestapp/ApacheRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahayavuz.apacherestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
