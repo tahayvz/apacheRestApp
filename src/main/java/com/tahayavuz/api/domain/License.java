1bc09c835419ca065049f9b47a1aa763e7c614b2
Taha Yavuz <45938952+tahayvz@users.noreply.github.com>
2021-06-27 22:33:07 +0300

    adding data model

---
+++ src/main/java/com/tahayavuz/api/domain/License.java
@@ -0,0 +1,9 @@
+package com.tahayavuz.api.domain;
+
+public class License {
+    public String key;
+    public String name;
+    public String spdx_id;
+    public String url;
+    public String node_id;
+}
---
+++ src/main/java/com/tahayavuz/api/domain/Permissions.java
@@ -0,0 +1,7 @@
+package com.tahayavuz.api.domain;
+
+public class Permissions {
+    public boolean admin;
+    public boolean push;
+    public boolean pull;
+}
